de648e45c7130a77251da05ee331e0fa9d08fb75
Omkar Mhatre <omkartemp502@gmail.com>
2021-07-17 12:53:58 +0530

    Add GridProblem

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+1) Grid-Problem
+   Say that you are travel on 2D grid. Yo begin in the top-left corner and your goal is to travel
+   to the bottom right corner. You may only move down or right.
+   In how many ways can you travel to the goal on a 2D grid with dimension m*n
+   (1,1) =1    (2,3) = 3  (3,2) = 3  (3,3) = 6
+   (18,18) = 2333606220
---
+++ src/com/company/GridProblem.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Say that you are travel on 2D grid. Yo begin in the top-left corner and your goal is to travel
+to the bottom right corner. You may only move down or right.
+In how many ways can you travel to the goal on a 2D grid with dimension m*n
+ */
+public class GridProblem {
+
+    public static void main(String[] args) {
+	System.out.println("hello");
+    }
+}
