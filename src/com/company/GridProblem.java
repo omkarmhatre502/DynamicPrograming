d220d2da0384cd6e65dfba1a52e2a3a22cf462a8
Omkar Mhatre <omkartemp502@gmail.com>
2021-07-17 12:59:16 +0530

    Update Gridproblem

--- src/com/company/GridProblem.java
+++ src/com/company/GridProblem.java
@@ -12,5 +12,23 @@ public class GridProblem {
 
     public static void main(String[] args) {
 	System.out.println("hello");
+        Map<String, Long> memo = new HashMap<String, Long>();
+        long ways=travel(18,18,memo);
+        System.out.println(ways);
+
+    }
+
+    private static long travel(int m, int n, Map<String, Long> memo) {
+        String key=m+","+n;
+        if( m==1 && n==1)
+            return 1;
+        if(m ==0 || n== 0)
+            return 0;
+        if(memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        long count = travel(m,n-1,memo) + travel(m-1,n,memo);
+        memo.put(key,count);
+        return count;
     }
 }
